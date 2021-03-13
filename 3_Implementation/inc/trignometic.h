ae2a80c1beef79fb608c4f63217aed0490781eb5
99003751 <78887234+99003751@users.noreply.github.com>
2021-03-13 09:27:28 +0530

    added trigonometric header file

---
+++ 3_Implementation/inc/trignometic.h
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+
+void trig_choose();
+
+double sine(double);
+double cosine(double);
+double tangent(double );
+double cot(double );
+double sec(double );
+double cosec(double );
+
